63181fc74c1b7f26256f4ddf24a85818798fa8c5
Alexander Kulnyow <alexanderkulnyow@yahoo.com>
2018-01-14 00:17:47 +0300

    add pagees

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var
     gulp            = require('gulp'),
-    sass            = require('gulp-sass'),
-    autoprefixer    = require('gulp-autoprefixer'),
-    sourcemaps      = require('gulp-sourcemaps'),
+    sass            = require('gulp-sass'), // препроцессор sass
+    autoprefixer    = require('gulp-autoprefixer'), // вендорные префексы css
+    sourcemaps      = require('gulp-sourcemaps'), // создание sourcemap
     concat          = require('gulp-concat'),
     browsersync     = require('browser-sync'),
     watch           = require('gulp-watch'),
-    imagemin        = require('gulp-imagemin');
+    imagemin        = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+    pngquant        = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+    del             = require('del'), // Подключаем библиотеку для удаления файлов и папок
+    cache           = require('gulp-cache'); // Подключаем библиотеку кеширования
 //переменные путей
 var path = {
     src: {
@@ -16,14 +19,17 @@ var path = {
         sass:   'src/sass/**/*.*',
         js:     'src/js/**/*.*',
         php:    'src/**/*.php',
-        img:    'src/img**/*.*'
+        img:    'src/img/**/*.*',
+        fonts:  'src/fonts**/*.*'
     },
     dist: {
-        pug:    'src/pug/*.*',
-        css:    'src/css/',
-        sass:   'src/sass/*/*.*',
-        js:     'src/js/*/*.*',
-        img:    'src/img**/*.*'
+        folders:'dist',
+        php:    'dist/**/*.php',
+        html:   'dist/*.html',
+        css:    'dist/css/',
+        js:     'dist/js/*/*.*',
+        img:    'dist/img**/*.*',
+        fonts:  'dist/fonts**/*.*'
     }
 };
 // tasks
@@ -47,8 +53,37 @@ gulp.task('watch', ['browsersync', 'sass'], function () {
     //gulp.watch(path.src.html, browsersync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch(path.src.php, browsersync.reload); // Наблюдение за HTML файлами в корне проекта
 });
-gulp.task('imagemin', function() {
-    return gulp.src(path.src.img)
-        .pipe(cache(imagemin())) // Cache Images
-        .pipe(gulp.dest('dist/img'));
+
+gulp.task('img', function() {
+    return gulp.src(path.src.img) // Берем все изображения из app
+        .pipe(cache(imagemin({ // С кешированием
+            // .pipe(imagemin({ // Сжимаем изображения без кеширования
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))/**/)
+        .pipe(gulp.dest(path.dist.img)); // Выгружаем на продакшен
 });
+gulp.task('clean', function() {
+    return del.sync(path.dist.folders); // Удаляем папку dist перед сборкой
+});
+gulp.task('build', ['clean', 'img', 'sass'], function() {
+
+    var buildCss = gulp.src(path.src.css)
+        .pipe(gulp.dest(path.dist.css))
+
+    var buildFonts = gulp.src(path.src.fonts) // Переносим шрифты в продакшен
+        .pipe(gulp.dest(path.dist.fonts))
+
+    var buildJs = gulp.src(path.src.js) // Переносим скрипты в продакшен
+        .pipe(gulp.dest(path.dist.js))
+
+    var buildHtml = gulp.src(path.src.html) // Переносим HTML в продакшен
+        .pipe(gulp.dest(path.dist.html));
+
+    var buildPHP = gulp.src(path.src.php) // Переносим PHP в продакшен
+        .pipe(gulp.dest(path.dist.php));
+});
+
+gulp.task('default', ['watch']);
